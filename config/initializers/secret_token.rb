baae1ff8260fab254b41cb3807bd31a4558f3606
James Kernan <jameskernanjr@gmail.com>
2013-02-18 12:16:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waevy::Application.config.secret_token = 'cc40f1d0ea20c4f94cba0452ef71d089c02ed1895d3901c24eccebebbb2f3aee651d51bca0f423dd8b1b13bf6b47cb3b8f9d5421ed638b677da6c6b37670e470'
